7d055464750248576b87b5f3b7aa9a0ad6b577c4
rng70 <1705070@ugrad.cse.buet.ac.bd>
2020-05-26 02:25:54 +0600

    updated repo

--- java/javaClassCodesExample/Enumeration/WrapDemo.java
+++
@@ -1,8 +0,0 @@
-
-public class WrapDemo {
-    public static void main(String args[]) {
-        Integer iOb = new Integer(100);
-        int i = iOb.intValue();
-        System.out.println(i + " " + iOb);
-    }
-}
